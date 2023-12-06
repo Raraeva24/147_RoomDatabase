66d5c178f57ba3711f03a99bac84f0067c870b2f
Rara Eva Maharani <rara.eva.ft21@mail.umy.ac.id>
2023-12-06 20:10:04 +0700

    Menambahkan file class "AplikasiSiswa"

---
+++ app/src/main/java/com/example/a147_roomdatabase/AplikasiSiswa.kt
@@ -0,0 +1,13 @@
+package com.example.a147_roomdatabase
+
+import android.app.Application
+import com.example.a147_roomdatabase.repositori.ContainerApp
+import com.example.a147_roomdatabase.repositori.ContainerDataApp
+
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate(){
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
